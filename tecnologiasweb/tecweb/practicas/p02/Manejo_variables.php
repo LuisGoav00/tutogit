d3c4c0256aaf063b552a6aea5107b6e898afe6af
luisgoav00 <luisgoav4717@gmail.com>
2022-09-12 11:36:25 -0500

    Resolvemos el ejercicio 2 de la practica

--- tecnologiasweb/tecweb/practicas/p02/Manejo_variables.php
+++ tecnologiasweb/tecweb/practicas/p02/Manejo_variables.php
@@ -18,6 +18,39 @@ $_element1 = 4;
     echo "<br>";
     echo "$_myvar,$myvar,$_7var,$_element1 <br>";
 
+    /*******************2*********************/
+echo "*******************2*********************<br><br>";
+$a = "ManejadorSQL2";
+$b = 'MySQL';
+$c = &$a;
+
+echo $a;
+echo "<br>";
+echo $b;
+echo "<br>";
+echo $c;
+echo "<br>";
+echo "<br>";
+
+$a = "PHP server";
+$b = &$a;
+echo "Mostrando contenido de nuevo... <br>";
+echo $a;
+echo "<br>";
+echo $b;
+echo "<br>";
+echo $c;
+echo "<br>";
+echo "¿Que sucedio?<br>";
+echo "Al agregar otro valor a la variable a, modificamos su contenido y
+y al referenciar la variable b a la variable a, estamos mostrando el valor
+de la variable a y como la variable c ya estaba referenciando a la variable a,
+todas las variables muestran el mismo resultado.<br>";
+unset($c);
+unset($b);
+unset($a);
+
+
 ?>
 
 
