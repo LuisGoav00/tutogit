639fd8ef874b10611d411f3a27b568d2ab566f95
luisgoav00 <luisgoav4717@gmail.com>
2022-10-03 18:30:32 -0500

    cambio

--- tecnologiasweb/tecweb/practicas/p04/funciones_index.php
+++ tecnologiasweb/tecweb/practicas/p04/funciones_index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Práctica 3</title>
+    <title>Práctica 4</title>
     <style type="text/css">
       ol { 
       list-style-type: none;
